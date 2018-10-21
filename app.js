0a8560c1bb8febd1293015d7dfa1d7ea87749930
Johai Kadosh <johaik@gmail.com>
2018-10-21 10:16:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
+
+// mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("semantic"));
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.listen(3000, "localhost", function () {
+    console.log("The Server Has Started!");
+});
